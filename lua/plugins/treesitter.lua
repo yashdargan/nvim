f30722a603dbcbaf64174294a590aad108059663
yashdargan <yashrajdargan@gmail.com>
2024-03-05 23:42:48 +0530

    fix: auto installation any lang encounter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,12 @@
-return { 
-  "nvim-treesitter/nvim-treesitter", 
+return {
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-   local config =require("nvim-treesitter.configs")
-   config.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html","python" },
-      sync_install = false,
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      auto_install = true,
       highlight = { enable = true },
-      indent = { enable = true },  
-  })
-end
+      indent = { enable = true },
+    })
+  end
 }
-
